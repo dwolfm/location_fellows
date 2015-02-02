c30966926ddf2b7b46bf6e1e611dd04727c46f3e
drwizzard <drwizzard@delia.local>
2015-02-02 12:55:51 -0800

    Initial Commit

---
+++ location_fellows/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  location_fellows
+//
+//  Created by drwizzard on 2/2/15.
+//  Copyright (c) 2015 nacnud. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ location_fellows/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  location_fellows
+//
+//  Created by drwizzard on 2/2/15.
+//  Copyright (c) 2015 nacnud. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
